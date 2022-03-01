a0f92080812e2719cb525143af613cbacc359697
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-01 13:01:09 +0100

    Add files via upload

--- 9/9.8 - 9.13/index.ts
+++ 9/9.8 - 9.13/index.ts
@@ -1,7 +1,9 @@
 import express  from "express";
 import cors from 'cors'
-const app = express()
+import diagnoseRouter from './src/routes/diagnoses'
+import patientsRouter from "./src/routes/patients";
 
+const app = express()
 const allowedOrigins = ['http://localhost:3000'];
 
 const options: cors.CorsOptions = {
@@ -15,8 +17,10 @@ const PORT = 3001
 
 app.get('/api/ping', (_req, res) => {
 res.send("PONG")
-    
-})
+    })
+
+app.use('/api/diagnoses', diagnoseRouter)
+app.use('/api/patients', patientsRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
