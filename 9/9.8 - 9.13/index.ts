79b1d8a77a81df08f790a68aab8fd10092dc5732
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-01 08:38:32 +0100

    Add files via upload

---
+++ 9/9.8 - 9.13/index.ts
@@ -0,0 +1,24 @@
+import express  from "express";
+import cors from 'cors'
+const app = express()
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options))
+app.use(express.json())
+
+const PORT = 3001
+
+app.get('/api/ping', (_req, res) => {
+res.send("PONG")
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    
+})
