42ef67b6b9cdae308c023228c1e4811e1dab9151
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-03 20:41:33 +0100

    Add files via upload

---
+++ 9/9.19 - 9.22/src/services/patientsServices.ts
@@ -0,0 +1,24 @@
+import patients from '../../data/patients';
+import { newPatient, NonSSNPatient, Patient } from '../types/Patient';
+
+const getAllPatients=():NonSSNPatient[]=>{
+return patients.map(({id, name, gender, dateOfBirth, occupation})=>({
+id,name, gender, dateOfBirth, occupation
+}));
+};
+
+const getSinglePatient = (id:string) :Patient => {
+const pat:any = patients.find(patient => patient.id === id);
+    return pat;
+    
+};
+
+const addPatient = (patient: newPatient) :newPatient=> {
+
+console.log(patient);
+
+patients.push(patient);
+return patient;
+};
+
+export default {getAllPatients,addPatient,getSinglePatient};
---
+++ 9/9.19 - 9.22/src/types/Patient.ts
@@ -0,0 +1,62 @@
+export interface Patient {
+    id: string,
+    name: string,
+    dateOfBirth: string,
+    ssn: string,
+    gender: string| Gender;
+    occupation: string;
+    entries?: Entry[]
+}
+
+
+export type NonSSNPatient = Omit<Patient , 'ssn' | 'entries'>;
+export type newPatient =  Omit<Patient ,  'entries'>;
+
+
+export enum Gender {
+    Male = "male",
+    Female = "female",
+    Other = "other"
+}
+
+
+export type Entry = HospitalEntry |  OccupationalHealthcareEntry
+| HealthCheckEntry;
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: string[];
+  }
+  interface OccupationalHealthcareEntry extends BaseEntry {
+    type:"OccupationalHealthcare"
+    employerName: string;
+    sickLeave?: SickLeave
+  }
+  interface HealthCheckEntry extends BaseEntry{
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+  }
+
+  interface HospitalEntry extends BaseEntry {
+      type: "Hospital"
+    discharge: Discharge
+  }
+
+  enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  interface SickLeave {
+      startDate: string;
+      endDate: string
+  }
+
+  interface Discharge {
+      date: string;
+      criteria: string
+  }
