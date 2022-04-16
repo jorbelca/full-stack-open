15c0beccd099ea320dede7b07ac130aa500f218b
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-04-16 19:25:40 +0200

    Add files via upload
    updated validation

--- 3/3.19 - 3.22/models/person.js
+++ 3/3.19 - 3.22/models/person.js
@@ -13,12 +13,20 @@ const personSchema = new mongoose.Schema({
     type: Number,
     required: true,
     unique: true,
-    validate: {
-      validator: function (val) {
-        return val.toString().length >= 8
+    validate: [
+      {
+        validator: function (val) {
+          return val.toString().length >= 8
+        },
+        message: (val) => `${val.value} has to have at least 8 digits`,
       },
-      message: (val) => `${val.value} has to have at least 8 digits`,
-    },
+      {
+        validator: function (v) {
+          return /\d{3}-\d{5}/.test(v) || /\d{2}-\d{6}/.test(v)
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    ],
   },
 })
 
